52dfacae6cf859603a34f9216b8bf57a056a6642
Karl Axel <Zerq_tm@hotmail.com>
2017-01-22 18:29:59 +0100

    Yay basic tile rendering works... now i can start making the representation of the player and indicating look direction and angle of view.

--- raycastTS/app/main.ts
+++ raycastTS/app/main.ts
@@ -2,41 +2,57 @@
 import { TileBase } from './model/tilebase';
 import { TileCollection } from './model/tilecollection';
 
-let grid = new Grid<number>(5, 5);
-grid.add(0, 0, 0);
-grid.add(0, 1, 0);
-grid.add(0, 2, 0);
-grid.add(0, 3, 0);
-grid.add(0, 4, 0);
-
-grid.add(1, 0, 0);
-grid.add(1, 1, null);
-grid.add(1, 2, null);
-grid.add(1, 3, null);
-grid.add(1, 4, 0);
-
-grid.add(2, 0, 0);
-grid.add(2, 1, null);
-grid.add(2, 2, null);
-grid.add(2, 3, null);
-grid.add(2, 4, 3);
+let grid = new Grid<number>(6, 6);
+grid.add(0,     0, 0);
+grid.add(0,     1, 0);
+grid.add(1,     2, 0);
+grid.add(4,     3, 0);
+grid.add(0,     4, 0);
+grid.add(0,     5, 0);
 
 
-grid.add(3, 0, 0);
-grid.add(3, 1, null);
-grid.add(3, 2, null);
-grid.add(3, 3, null);
-grid.add(3, 4, 0);
 
-grid.add(4, 0, 0);
-grid.add(4, 1, 0);
-grid.add(4, 2, 1);
-grid.add(4, 3, 0);
-grid.add(4, 4, 0);
+grid.add(0,     0, 1);
+grid.add(null,  1, 1);
+grid.add(null,  2, 1);
+grid.add(null,  3, 1);
+grid.add(null,  4, 1);
+grid.add(0,     5, 1);
+
+grid.add(1,     0, 2);
+grid.add(null,  1, 2);
+grid.add(null,  2, 2);
+grid.add(null,  4, 2);
+grid.add(3,     5, 2);
+
+
+grid.add(0,     0, 3);
+grid.add(null,  1, 3);
+grid.add(null,  2, 3);
+grid.add(3,     3, 3);
+grid.add(null,  4, 3);
+grid.add(3,     5, 3);
+
+
+grid.add(0,     0, 4);
+grid.add(1,     1, 4);
+grid.add(0,     2, 4);
+grid.add(null,     3, 4);
+grid.add(null,     4, 4);
+grid.add(1,     5, 4);
+
+
+grid.add(0, 0, 5);
+grid.add(1, 1, 5);
+grid.add(0, 2, 5);
+grid.add(0, 3, 5);
+grid.add(1, 4, 5);
+grid.add(1, 5, 5);
+
 
 let canvas = <HTMLCanvasElement>document.getElementById("game");
 var context = canvas.getContext("2d");
- 
+
 var img = <HTMLCanvasElement>document.getElementById("img");
 context.clearRect(0, 0, 64 * 5, 64 * 5);
 grid.gridTraverse((n, x, y) => {
@@ -46,6 +62,6 @@ grid.gridTraverse((n, x, y) => {
     }
 }, () => {
 
-    });
+});
 
 //14kg, vesa200
